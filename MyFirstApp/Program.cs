26fee452657f48cf39361f13149af64d64f5dc64
Simon O <simonoja23@gmail.com>
2022-09-03 17:17:19 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Sisesta oma nimi:"); //Output
+//String - sõne
+String Kasutajanimi = Console.ReadLine(); //Input
+
+Console.WriteLine("Tere" + ", " + Kasutajanimi + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Tere, {Kasutajanimi} !"); //Output
